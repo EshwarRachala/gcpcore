3419b3c539cc61570e7a4717dcdce65f954860d6
eshwarrachala <rachala2001@gmaill.com>
2018-08-26 12:59:12 -0400

    implemented logging

--- Program.cs
+++ Program.cs
@@ -1,59 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Serilog;
+﻿using System; 
+using System.Collections.Generic; 
+using System.IO; 
+using System.Linq; 
+using System.Threading.Tasks; 
+using Microsoft.AspNetCore; 
+using Microsoft.AspNetCore.Hosting; 
+using Microsoft.Extensions.Configuration; 
+using Microsoft.Extensions.Logging; 
+using Serilog; 
 
 namespace localmarket {
     public class Program {
         public static void Main (string[] args) {
-            Log.Logger = new LoggerConfiguration ()
+            Log.Logger = 
+             new LoggerConfiguration ()
                 .MinimumLevel.Debug ()
                 .WriteTo.Console ()
                 .WriteTo.Seq ("http://localhost:5341")
-                .WriteTo.File ("log.clef", rollingInterval : RollingInterval.Day,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
-                .CreateLogger ();
+                .WriteTo.File ("log.clef", rollingInterval:RollingInterval.Day, outputTemplate:"{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger (); 
 
-            Log.Information ("Hello, world!");
+            Log.Information ("Local Market webhost is starting!"); 
 
-            BuildWebHost (args).Run ();
+            BuildWebHost (args).Run (); 
 
-            Log.CloseAndFlush ();
+            Log.Information ("Local Market webhost is successfully started!"); 
+
+            Log.CloseAndFlush (); 
         }
 
-        public static IWebHost BuildWebHost (string[] args) =>
+        public static IWebHost BuildWebHost (string[] args) => 
             WebHost.CreateDefaultBuilder (args)
             .UseKestrel ()
             .UseContentRoot (Directory.GetCurrentDirectory ())
-            .ConfigureAppConfiguration ((builderContext, config) => {
+            .ConfigureAppConfiguration ((builderContext, config) =>  {
 
-                IHostingEnvironment env = builderContext.HostingEnvironment;
+                IHostingEnvironment env = builderContext.HostingEnvironment; 
 
                 config.SetBasePath (Directory.GetCurrentDirectory ())
-                    .AddJsonFile ("appsettings.json", optional : true, reloadOnChange : true)
-                    .AddJsonFile ($"appsettings.{env.EnvironmentName}.json", optional : true, reloadOnChange : true)
-                    .AddEnvironmentVariables ();
+                    .AddJsonFile ("appsettings.json", optional:true, reloadOnChange:true)
+                    .AddJsonFile ($"appsettings.{env.EnvironmentName}.json", optional:true, reloadOnChange:true)
+                    .AddEnvironmentVariables (); 
 
                 if (args != null) {
-                    config.AddCommandLine (args);
+                    config.AddCommandLine (args); 
                 }
             })
-            .ConfigureLogging ((hostingContext, logging) => {
-                logging.AddConfiguration (hostingContext.Configuration.GetSection ("Logging"));
-                logging.AddConsole ();
-                logging.AddDebug ();
-            })
             .UseIISIntegration ()
-            .UseDefaultServiceProvider ((context, options) => {
-                options.ValidateScopes = context.HostingEnvironment.IsDevelopment ();
+            .UseDefaultServiceProvider ((context, options) =>  {
+                options.ValidateScopes = context.HostingEnvironment.IsDevelopment (); 
             })
-            .UseStartup<Startup> ()
-            .Build ();
+            .UseStartup < Startup > ()
+            .Build (); 
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,117 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Swashbuckle.AspNetCore.Swagger;
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Net; 
+using System.Security.Claims; 
+using System.Threading.Tasks; 
+using Microsoft.AspNetCore.Authentication.JwtBearer; 
+using Microsoft.AspNetCore.Builder; 
+using Microsoft.AspNetCore.Hosting; 
+using Microsoft.AspNetCore.Http; 
+using Microsoft.Extensions.Configuration; 
+using Microsoft.Extensions.DependencyInjection; 
+using Microsoft.Extensions.Logging; 
+using Microsoft.Extensions.Options; 
+using Microsoft.IdentityModel.Tokens; 
+using Swashbuckle.AspNetCore.Swagger; 
 
 namespace localmarket {
     public class Startup {
         public Startup (IConfiguration configuration) {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration {get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
-            services.AddMvc ();
+            services.AddMvc (); 
 
-            services.AddLogging ();
+            services.AddLogging (); 
             // Register the Swagger generator
-            services.AddSwaggerGen (c => {
+            services.AddSwaggerGen (c =>  {
 
                 c.SwaggerDoc ("v1", new Info {
-                    Version = "v1",
-                        Title = "Local Market API",
-                        Description = "Local Market API",
-                        TermsOfService = "None",
-                        Contact = new Contact {
-                            Name = "Eshwar Rachala",
-                                Email = string.Empty,
-                                Url = "https://twitter.com/eswarrachala"
-                        },
-                        License = new License {
-                            Name = "Local market",
-                                Url = "https://eshwarrachala.com/"
-                        }
-                });
+                    Version = "v1", 
+                    Title = "Local Market API", 
+                    Description = "Local Market API", 
+                    TermsOfService = "None", 
+                    Contact = new Contact {Name = "Eshwar Rachala", Email = string.Empty, Url = "https://twitter.com/eswarrachala"}, 
+                    License = new License {Name = "Local market", Url = "https://eshwarrachala.com/"}
+                }); 
 
                 c.AddSecurityDefinition ("Bearer", new ApiKeyScheme {
-                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                        Name = "Authorization",
-                        In = "header",
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization:Bearer {token}\"", 
+                        Name = "Authorization", 
+                        In = "header", 
                         Type = "apiKey"
-                });
-                c.AddSecurityRequirement (new Dictionary<string, IEnumerable<string>> { { "Bearer", new string[] { } },
-                });
+                }); 
+                c.AddSecurityRequirement (new Dictionary < string, IEnumerable < string >>  { {"Bearer", new string[] {}}, 
+                }); 
                 // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 // var xmlPath = Path.Combine (AppContext.BaseDirectory, xmlFile);
                 // c.IncludeXmlComments (xmlPath);
-            });
+            }); 
 
-            services.AddCors (options => {
-                options.AddPolicy ("AllowEverything",
+            services.AddCors (options =>  {
+                options.AddPolicy ("AllowEverything", 
                     builder => builder
                     .AllowAnyHeader ()
                     .AllowAnyMethod ()
                     .AllowAnyOrigin ()
                     .AllowCredentials ()
-                );
-            });
+                ); 
+            }); 
 
             services
                 .AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer (options => {
-                    options.Authority = Configuration["JWT:Issuer"];
+                .AddJwtBearer (options =>  {
+                    options.Authority = Configuration["JWT:Issuer"]; 
                     options.TokenValidationParameters = new TokenValidationParameters {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = Configuration["JWT:Issuer"],
+                        ValidateIssuer = true, 
+                        ValidateAudience = true, 
+                        ValidateLifetime = true, 
+                        ValidateIssuerSigningKey = true, 
+                        ValidIssuer = Configuration["JWT:Issuer"], 
                         ValidAudience = Configuration["JWT:Audience"]
-                    };
-                });
+                    }; 
+                }); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
+                app.UseDeveloperExceptionPage (); 
             }
+            
+            app.UseCors ("AllowEverything"); 
 
-            loggerFactory.AddConsole (Configuration.GetSection ("Logging"));
-            loggerFactory.AddDebug ();
-
-            app.UseCors ("AllowEverything");
-
-            app.UseStaticFiles ();
+            app.UseStaticFiles (); 
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger ();
+            app.UseSwagger (); 
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI (c => {
-                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "Local Market API V1");
-                c.RoutePrefix = string.Empty;
-                c.DocumentTitle = "Title Documentation";
-                c.DocExpansion (DocExpansion.List);
-            });
+            app.UseSwaggerUI (c =>  {
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "Local Market API V1"); 
+                c.RoutePrefix = string.Empty; 
+                c.DocumentTitle = "Title Documentation"; 
+                c.DocExpansion (DocExpansion.List); 
+            }); 
 
-            app.UseAuthentication ();
-            //  app.UseMvcWithDefaultRoute ();
-            app.UseMvc ();
+            app.UseAuthentication (); 
+            app.UseMvc (); 
         }
     }
 }
